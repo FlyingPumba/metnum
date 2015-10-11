93b9d2db9a4f9c9dfc068a0f817d788d7814542d
Ivan Arcuschin <iarcuschin@gmail.com>
2015-10-11 19:05:25 -0300

    Cambio menor a Taller 2

--- Taller2-20152C/jacobi_matricial.m
+++ Taller2-20152C/jacobi_matricial.m
@@ -1,9 +1,9 @@
-function [X] = gausssei(A,b,x0)
+function [X] = jacobi_matricial(A,b,x0)
     D = diag(diag(A));
     L = tril(-A, -1);
     U = triu(-A, 1);
-    C = inv(D - L)*b;
-    T = inv(D - L)*U;
+    C = inv(D)*b;
+    T = inv(D)*(L+U);
     X = x0;
     i = 0;
     while (norm(A*X - b) > 0.000001)
